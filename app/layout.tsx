8104ce730539dc3b689b28811c345bc11e5e699c
Dika P. <dikapramudia11@gmail.com>
2024-02-09 16:54:48 +0700

    penambahan adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,14 +9,14 @@ import { Toaster } from 'react-hot-toast';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-	title: 'Dika | Personal Portfolio',
-	description: 'Dika is a Fullstack developer with 3 years of experience.',
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en' className='!scroll-smooth'>
+			<head>
+				<title>Dika | Personal Portfolio</title>
+				<meta name='description' content='Dika is a Fullstack developer with 3 years of experience.' />
+				<meta name='google-adsense-account' content='ca-pub-5295382362712798' />
+			</head>
 			<body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
 				<div className='bg-[#0091ff] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#0091ff]'></div>
 				<div className='bg-[#ff0005] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#ff0005]'></div>
