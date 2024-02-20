9a400401c5f263c552243b01ec4f0f43396ebc04
Dika P. <dikapramudia11@gmail.com>
2024-02-20 16:17:09 +0700

    fixing 01

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: true,
-  },
+	output: 'export',
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'images.unsplash.com',
+			},
+		],
+	},
+	experimental: {
+		serverActions: true,
+	},
 };
 
 module.exports = nextConfig;
